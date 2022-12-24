157b89d658991c2086843e753e05a9173b8b0a49
yusupovoff <yusardor199504@gmail.com>
2022-12-25 00:20:28 +0500

    fixed bag

--- Js_task_29/main.js
+++ Js_task_29/main.js
@@ -3,7 +3,7 @@ let yearInp_one=document.getElementById("yearInp").value
 let monthInp_two=document.getElementById("monthInp").value
 function kabisaYili(yearInp_one,monthInp_two){
     if (yearInp_one%4===0){
-        document.getElementById("yearKabisa").innerHTML=(yearInp_one,"bu yil kabisa yili");
+        document.getElementById("yearKabisa").innerHTML=(yearInp,"Bu yil kabisa yili...");
         switch (monthInp_two){
             case "yanvar":
             case "mart":
@@ -12,24 +12,24 @@ function kabisaYili(yearInp_one,monthInp_two){
             case "avgust":
             case "oktabr":
             case "dekabr":
-                document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 31 kun bor");
+                document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 31 kun bor...");
                 break;
             case "fevral":
-                document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 29 kun bor");
+                document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 29 kun bor...");
                 break;
             case "aprel":
             case "iyun":
             case "sentabr":
             case "noyabr":
-                document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 30 kun bor");
+                document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 30 kun bor...");
                 break;
             default :
-            document.getElementById("answer").innerHTML=(monthInp_two,"bunday oy yo'q");
+            document.getElementById("answer").innerHTML=(monthInp_two,"Bunday oy yo'q...");
                 break;
         }
     }
     else {
-        document.getElementById("yearKabisa").innerHTML=(yearInp_one,"kabisa yili emas");
+        document.getElementById("yearKabisa").innerHTML=(yearInp.value,"Kabisa yili emas...");
         switch (monthInp_two){
         case "yanvar":
         case "mart":
@@ -38,19 +38,19 @@ function kabisaYili(yearInp_one,monthInp_two){
         case "avgust":
         case "oktabr":
         case "dekabr":
-            document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 31 kun bor");
+            document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 31 kun bor...");
             break;
         case "fevral":
-            document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 28 kun bor");
+            document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 28 kun bor...");
             break;
         case "aprel":
         case "iyun":
         case "sentabr":
         case "noyabr":
-        document.getElementById("answer").innerHTML=(monthInp_two,"bu oyda 30 kun bor");
+        document.getElementById("answer").innerHTML=(monthInp_two,"Bu oyda 30 kun bor...");
             break;
             default :
-        console.log("bunday yoq");
+        console.log("Bunday yoq...");
                 break;
         }
     }
