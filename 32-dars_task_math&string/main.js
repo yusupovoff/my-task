4aaeb3c99b67f6820094f0120587467c95bf141c
yusupovoff <yusardor199504@gmail.com>
2022-12-26 23:25:27 +0500

    added task

---
+++ 32-dars_task_math&string/main.js
@@ -0,0 +1,20 @@
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.SQRT1_2);
+// console.log(Math.sqrt(144));
+// console.log(Math.abs(-7));
+console.log(Math.round(3.6));
+console.log(Math.ceil(3.2));
+console.log(Math.floor(3.8));
+console.log(Math.min(3,54,2,332,56));
+console.log(Math.max(3,54,2,332,56));
+// math obyektlari
+let text="Hello world"
+// console.log(text.toUpperCase());
+// console.log(text.toLowerCase());
+// console.log(text.trim());
+// console.log(text.includes("lo"));
+// console.log(text.indexOf("r"),'-index da');
+// console.log(text.replace("Hello","salom"));
+// console.log(text.slice(3, 7));
+console.log(text.split(" "));
---
+++ 33-dars_task_arraymeth/main.js
@@ -0,0 +1,4 @@
+const fruits=[ "banana", "orange", "Aplle", "Mango"];
+// fruits.push("anor","olma");
+fruits.splice(1, 2, "Kiwi", "pich", "Chery")
+console.log(fruits);
